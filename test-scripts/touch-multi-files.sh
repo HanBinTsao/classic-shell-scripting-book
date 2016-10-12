5b334d1be6b5b0a6336d2782823b18e3d45c379c
HanBinTsao <hbtsao@gmail.com>
2016-10-13 01:10:10 +0800

    Update new test scripts

---
+++ test-scripts/touch-multi-files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -
+#
+# Touch multiple files
+#
+# Usage ./touch-multi-files.sh
+#
+# Result: file1 file2 file3 ...
+#
+IFS='
+ 	'
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
+
+for i in (1..5
